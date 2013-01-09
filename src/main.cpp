9dc56f97589b9a0dadab061b4c896536f827a0ab
Chat-Wane <brice.nedelec@gmail.com>
2013-01-09 23:36:58 +0100

    main Window

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <SFML/Window.hpp>
 
 using namespace std;
 
 int main()
 {
-  std::cout << "Dat iz a tasty burger" << std::endl;
+  sf::Window window(sf::VideoMode(800, 600), "Gravedigger");
+  //std::cout << "Dat iz a tasty burger" << std::endl;
+
+  while (window.isOpen()) {
+    sf::Event event;
+
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+	window.close();
+      }
+    }
+  }
+
+  return 0;
 }
 
