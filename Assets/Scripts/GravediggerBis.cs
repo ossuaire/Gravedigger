9a90ff192082304a07b1d2135ad144cc573e5abe
Adrien Drouet <drizz764@gmail.com>
2013-12-17 22:14:04 +0100

    improve physics #11
    + refactoring scene

---
+++ Assets/Scripts/GravediggerBis.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class GravediggerBis : MonoBehaviour
+{
+	// movement config
+	public float gravity = -15f;
+	public float runSpeed = 8f;
+	public float groundDamping = 20f; // how fast do we change direction? higher means faster
+	public float inAirDamping = 5f;
+	public float jumpHeight = 3f;
+	
+	[HideInInspector]
+	private float _rawMovementDirection = 1;
+	[HideInInspector]
+	private float normalizedHorizontalSpeed = 0;
+	
+	private CharacterController2D _controller;
+	private Animator _animator;
+	private RaycastHit2D _lastControllerColliderHit;
+	private Vector3 _velocity;
+	
+	
+	void Awake()
+	{
+		_animator = GetComponent<Animator>();
+		_controller = GetComponent<CharacterController2D>();
+		_controller.onControllerCollidedEvent += onControllerCollider;
+	}
+	
+	
+	void onControllerCollider( RaycastHit2D hit )
+	{
+		// bail out on plain old ground hits
+		if( hit.normal.y == 1f )
+			return;
+		
+		// logs any collider hits if uncommented
+		//Debug.Log( "flags: " + _controller.collisionState + ", hit.normal: " + hit.normal );
+	}
+	
+	
+	// the Update loop contains a very simple example of moving the character around and controlling the animation
+	void Update()
+	{
+		// grab our current _velocity to use as a base for all calculations
+		_velocity = _controller.velocity;
+		
+		if( _controller.isGrounded )
+			_velocity.y = 0;
+		
+		if( Input.GetKey( KeyCode.RightArrow ) )
+		{
+			normalizedHorizontalSpeed = 1;
+			if( transform.localScale.x < 0f )
+				transform.localScale = new Vector3( -transform.localScale.x, transform.localScale.y, transform.localScale.z );
+			
+			//if( _controller.isGrounded )
+				//_animator.Play( Animator.StringToHash( "Run" ) );
+		}
+		else if( Input.GetKey( KeyCode.LeftArrow ) )
+		{
+			normalizedHorizontalSpeed = -1;
+			if( transform.localScale.x > 0f )
+				transform.localScale = new Vector3( -transform.localScale.x, transform.localScale.y, transform.localScale.z );
+			
+			if( _controller.isGrounded )
+				_animator.SetTrigger("Grounded");
+		}
+		else
+		{
+			normalizedHorizontalSpeed = 0;
+			
+			//if( _controller.isGrounded )
+			//	_animator.Play( Animator.StringToHash( "Idle" ) );
+		}
+		
+		
+		// we can only jump whilst grounded
+		if( _controller.isGrounded && Input.GetKeyDown( KeyCode.UpArrow ) )
+		{
+			_velocity.y = Mathf.Sqrt( 2f * jumpHeight * -gravity );
+			_animator.SetTrigger("Jump");
+		}
+		
+		
+		// apply horizontal speed smoothing it
+		var smoothedMovementFactor = _controller.isGrounded ? groundDamping : inAirDamping; // how fast do we change direction?
+		_velocity.x = Mathf.Lerp( _velocity.x, normalizedHorizontalSpeed * _rawMovementDirection * runSpeed, Time.deltaTime * smoothedMovementFactor );
+		
+		// apply gravity before moving
+		_velocity.y += gravity * Time.deltaTime;
+		
+		_controller.move( _velocity * Time.deltaTime );
+
+		_animator.SetFloat("SpeedX", Mathf.Abs(_velocity.x));
+		_animator.SetFloat("SpeedY", _velocity.y);
+		_animator.SetBool ("Grounded",_controller.isGrounded);
+
+	}
+	
+}
