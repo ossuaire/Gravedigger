35fc78d0f692a5ace640686ee25b24a2e8847993
Adrien Drouet <drizz764@gmail.com>
2014-10-16 23:57:07 +0200

    Avoid endless fall

---
+++ Assets/Scripts/Fall.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("Dans le vide là !!");
+		Application.LoadLevel ("SandboxDriz");
+	}
+}
